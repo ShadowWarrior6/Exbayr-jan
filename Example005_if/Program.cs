3ff3d22b679c63a9d971d687419cf573dd32a51b
Andranik <andran1401@gmail.com>
2023-02-26 15:43:26 +0400

    Особенное привецтвие

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввидите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower()== "hasmik")
+{
+    Console.WriteLine("You are very beautiful Hasmik");
+}
+else 
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
